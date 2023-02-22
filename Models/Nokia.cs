0c44ade83391b55008fe0edc924fd645f6a5010b
sabrinagoes <66102016+sabrinagoes@users.noreply.github.com>
2023-02-21 22:00:27 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
+    public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
     public class Nokia
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia");
     }
 }
