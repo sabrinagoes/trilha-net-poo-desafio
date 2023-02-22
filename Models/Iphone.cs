b392ac1e38c3b26d686642e5033a9a45fa42d801
sabrinagoes <66102016+sabrinagoes@users.noreply.github.com>
2023-02-21 22:02:20 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
+    public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
     public class Iphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone");
     }
 }
